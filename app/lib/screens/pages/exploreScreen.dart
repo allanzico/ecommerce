1cdb42b90e577113161b0d3e8256d53d12e55e09
Allan <akanyijuka@gmail.com>
2020-07-29 20:45:00 +0200

    clean up

--- app/lib/screens/pages/exploreScreen.dart
+++ app/lib/screens/pages/exploreScreen.dart
@@ -10,126 +10,144 @@ class _ExploreScreenState extends State<ExploreScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        padding: EdgeInsets.symmetric(horizontal: 16, vertical: 15),
-        child: Column(
-          children: <Widget>[
-            Text(
-              "All Foods",
-              style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
-            ),
-            Container(
-              margin: EdgeInsets.all(10),
-              width: 200,
-              height: 200,
-              decoration: BoxDecoration(boxShadow: [
-                BoxShadow(
-                    color: Colors.grey[300],
-                    offset: Offset(1, 1),
-                    blurRadius: 2),
-              ], borderRadius: BorderRadius.circular(10)),
-              child: Stack(
-                alignment: Alignment.topCenter,
-                children: <Widget>[
-                  Container(
-                    child: Stack(
-                      children: <Widget>[
-                        Image(
-                          width: 200,
-                          image: AssetImage("assets/images/veal-chop.jpg"),
-                          fit: BoxFit.cover,
-                        ),
-                      ],
-                    ),
+      body: SingleChildScrollView(
+        child: Container(
+          padding: EdgeInsets.symmetric(horizontal: 10, vertical: 20),
+          child: Column(
+            children: <Widget>[
+              Text(
+                "All Foods",
+                style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+              ),
+              ClipRRect(
+                borderRadius:
+                    BorderRadius.only(bottomRight: Radius.circular(50)),
+                child: Container(
+                  margin: EdgeInsets.all(10),
+                  width: MediaQuery.of(context).size.width,
+                  height: 250,
+                  decoration: BoxDecoration(
+                    boxShadow: [
+                      BoxShadow(
+                          color: Colors.grey[300],
+                          offset: Offset(1, 1),
+                          blurRadius: 2),
+                    ],
                   ),
-                  Positioned(
-                    bottom: 0.0,
-                    child: Container(
-                      width: 200,
-                      decoration: BoxDecoration(
-                        color: Colors.white,
-                      ),
-                      child: Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          mainAxisAlignment: MainAxisAlignment.center,
+                  child: Stack(
+                    alignment: Alignment.topCenter,
+                    children: <Widget>[
+                      Container(
+                        child: Stack(
                           children: <Widget>[
-                            Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: <Widget>[
-                                Text(
-                                  "EBINYEBWA",
-                                  style: TextStyle(
-                                    fontSize: 16,
-                                    fontWeight: FontWeight.bold,
-                                    letterSpacing: 3,
-                                  ),
-                                ),
-                                Text(
-                                  "Just some small legumes",
-                                  style: TextStyle(
-                                      fontSize: 12, color: Colors.grey),
-                                ),
-                              ],
-                            ),
-                            Divider(),
-                            Row(
-                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                              children: <Widget>[
-                                Text(
-                                  "UGX 20,000",
-                                  style: TextStyle(
-                                      fontSize: 16,
-                                      fontWeight: FontWeight.bold,
-                                      color: Colors.green),
-                                ),
-                                // Container(
-                                //   width: 30,
-                                //   height: 30,
-                                //   child: Stack(
-                                //     children: <Widget>[
-                                //       Positioned(
-                                //           bottom: 0,
-                                //           right: 0,
-                                //           child: Container(
-                                //             width: 30,
-                                //             height: 30,
-                                //             decoration: BoxDecoration(
-                                //               color: Colors.green,
-                                //             ),
-                                //           ))
-                                //     ],
-                                //   ),
-                                // ),
-                              ],
+                            Image(
+                              width: MediaQuery.of(context).size.width,
+                              image: AssetImage("assets/images/veal-chop.jpg"),
+                              fit: BoxFit.cover,
                             ),
                           ],
                         ),
                       ),
-                    ),
-                  ),
-                  Positioned(
-                      bottom: 0,
-                      right: 0,
-                      child: InkWell(
-                        onTap: () {},
+                      Positioned(
+                        bottom: 0.0,
+                        left: 0,
                         child: Container(
-                          width: 40,
-                          height: 35,
+                          width: MediaQuery.of(context).size.width,
                           decoration: BoxDecoration(
-                            color: Colors.green,
-                          ),
-                          child: Icon(
-                            Icons.add_shopping_cart,
                             color: Colors.white,
                           ),
+                          child: Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: <Widget>[
+                                Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: <Widget>[
+                                    Text(
+                                      "EBINYEBWA",
+                                      style: TextStyle(
+                                        fontSize: 20,
+                                        fontWeight: FontWeight.bold,
+                                        letterSpacing: 3,
+                                      ),
+                                    ),
+                                    Text(
+                                      "Just some small legumes",
+                                      style: TextStyle(
+                                          fontSize: 14, color: Colors.grey),
+                                    ),
+                                  ],
+                                ),
+                                Divider(),
+                                SizedBox(
+                                  height: 10,
+                                ),
+                                Row(
+                                  mainAxisAlignment: MainAxisAlignment.start,
+                                  children: <Widget>[
+                                    Text(
+                                      "UGX 20,000",
+                                      style: TextStyle(
+                                          fontSize: 20,
+                                          fontWeight: FontWeight.bold,
+                                          color: Colors.green),
+                                    ),
+                                    // Container(
+                                    //   width: 30,
+                                    //   height: 30,
+                                    //   child: Stack(
+                                    //     children: <Widget>[
+                                    //       Positioned(
+                                    //           bottom: 0,
+                                    //           right: 0,
+                                    //           child: Container(
+                                    //             width: 30,
+                                    //             height: 30,
+                                    //             decoration: BoxDecoration(
+                                    //               color: Colors.green,
+                                    //             ),
+                                    //           ))
+                                    //     ],
+                                    //   ),
+                                    // ),
+                                  ],
+                                ),
+                              ],
+                            ),
+                          ),
                         ),
-                      )),
-                ],
-              ),
-            )
-          ],
+                      ),
+                      Positioned(
+                          bottom: 0,
+                          right: 0,
+                          child: InkWell(
+                            onTap: () {},
+                            child: Container(
+                              width: 50,
+                              height: 50,
+                              decoration: BoxDecoration(
+                                color: Colors.green,
+                                boxShadow: [
+                                  BoxShadow(
+                                      color: Colors.grey[300],
+                                      offset: Offset(-2, 1),
+                                      blurRadius: 5),
+                                ],
+                              ),
+                              child: Icon(
+                                Icons.add_shopping_cart,
+                                color: Colors.white,
+                              ),
+                            ),
+                          )),
+                    ],
+                  ),
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
